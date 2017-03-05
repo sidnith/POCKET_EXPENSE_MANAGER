9e9cf317fcf06f4c8de7e13eefbe0491e4f6a3dd
Siddharth <sidharthaggarwal3@gmail.com>
2017-03-05 18:03:32 +0530

    first commit

---
+++ app/src/main/java/com/example/sidharth/pocketexpensemanager/toolbar.java
@@ -0,0 +1,35 @@
+package com.example.sidharth.pocketexpensemanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Sidharth on 3/5/2017.
+ */
+
+public class toolbar extends AppCompatActivity{
+    Toolbar toolbar;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    viewPagerAdapter viewPagerAdapter;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tabLayout=(TabLayout)findViewById(R.id.tablayout);
+        viewPager=(ViewPager)findViewById(R.id.viewpager);
+        viewPagerAdapter=new viewPagerAdapter(getSupportFragmentManager());
+       viewPagerAdapter.addFragments(new HomeFragment(),"Home");
+        viewPagerAdapter.addFragments(new topfree(),"TopFree");
+        viewPagerAdapter.addFragments(new toppaid(),"TopPaid");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
