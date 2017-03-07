5026a2a33235d5c798b98d2456e693af02ee8f13
Siddharth <sidharthaggarwal3@gmail.com>
2017-03-08 00:21:17 +0530

    correct errors

--- app/src/main/java/com/example/sidharth/pocketexpensemanager/Expense.java
+++ app/src/main/java/com/example/sidharth/pocketexpensemanager/Expense.java
@@ -23,7 +23,7 @@ public class Expense extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_Expense, container, false);
+        return inflater.inflate(R.layout.fragment_expense, container, false);
     }
 
 }
